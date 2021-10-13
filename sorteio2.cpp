de544efcd7e36c1701ac7b0dd1844688e5a60370
Matheus Bonnet <82242779+MatheusBonnet@users.noreply.github.com>
2021-10-13 20:15:34 -0300

    Add files via upload

---
+++ sorteio2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+#define LIMITE 100000
+
+int dado();
+int main(){
+	
+	int numero = 0;
+	
+	std::cout << "Digite um numero para eu adivinhar: ";
+	std::cin >> numero;
+	
+	int result = dado();
+	std::cout << result;
+	
+	
+}
+
+int dado(){
+	
+    srand(time(NULL));
+    int sorteio = rand() % LIMITE;
+    return sorteio;
+
+}
